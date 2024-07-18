fc9e578d1d21d4eabead97ba63df95b099a842f3
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2024-07-18 23:05:26 +0530

    Update start.py

--- DAXXMUSIC/plugins/bot/start.py
+++ DAXXMUSIC/plugins/bot/start.py
@@ -28,14 +28,14 @@ from strings import get_string
 
 #--------------------------
 
-NEXI_VID = [
-"https://telegra.ph/file/1a3c152717eb9d2e94dc2.mp4",
-"https://graph.org/file/ba7699c28dab379b518ca.mp4",
-"https://graph.org/file/83ebf52e8bbf138620de7.mp4",
-"https://graph.org/file/82fd67aa56eb1b299e08d.mp4",
-"https://graph.org/file/318eac81e3d4667edcb77.mp4",
-"https://graph.org/file/7c1aa59649fbf3ab422da.mp4",
-"https://graph.org/file/2a7f857f31b32766ac6fc.mp4",
+NEXI_IMG = [
+"https://te.legra.ph/file/7757731c3e8b784b6a550.png",
+"https://te.legra.ph/file/7757731c3e8b784b6a550.png",
+"https://te.legra.ph/file/7757731c3e8b784b6a550.png",
+"https://te.legra.ph/file/7757731c3e8b784b6a550.png",
+"https://te.legra.ph/file/7757731c3e8b784b6a550.png",
+"https://te.legra.ph/file/7757731c3e8b784b6a550.png",
+"https://te.legra.ph/file/7757731c3e8b784b6a550.png",
 
 ]
 
@@ -50,7 +50,7 @@ async def start_pm(client, message: Message, _):
         if name[0:4] == "help":
             keyboard = help_pannel(_)
             return await message.reply_video(
-                random.choice(NEXI_VID),
+                random.choice(NEXI_IMG),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
